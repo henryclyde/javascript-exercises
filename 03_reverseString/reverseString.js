1b047ec41a86b2c88fa7c2fb769f8d6cc51e35ba
Henry Clyde <22669531+henryclyde@users.noreply.github.com>
2024-02-15 14:33:07 -0700

    exercises 1-3 completed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str = "hello there") {
+    let reverse = "";
+    for (let i = str.length; i >= 0; i--) {
+        reverse += str.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
