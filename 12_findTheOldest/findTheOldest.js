7fbc4594da79c12308f3329dcbc6ef1e2789965f
Henry Clyde <22669531+henryclyde@users.noreply.github.com>
2024-04-19 12:54:18 -0600

    finished 12, last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((b,a) => {
+        return (((a.yearOfDeath !== undefined ? a.yearOfDeath : 2024) - a.yearOfBirth) - ((b.yearOfDeath !== undefined ? b.yearOfDeath : 2024) - b.yearOfBirth));
+    });
+    return people[0];
 };
 
 // Do not edit below this line
+
 module.exports = findTheOldest;
